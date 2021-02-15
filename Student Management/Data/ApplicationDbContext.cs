4b4f06cc639987662bedb10f19b560be375d5a58
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-02-15 22:57:19 +0100

    updates

---
+++ Student Management/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Student_Management.Models;
+
+namespace Student_Management.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student_Management.Models.Student> Student { get; set; }
+    }
+}
---
+++ Student Management/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Models
+{
+    public class Student
+    {
+
+        public int id { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string CIN { get; set; }
+        public string adresse { get; set; }
+    }
+}
