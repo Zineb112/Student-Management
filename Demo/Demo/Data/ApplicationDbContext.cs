110864ce61e821b2bf3ea6927a77b9f8c8e4e43a
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-02-23 11:45:10 +0100

    new Demo

---
+++ Demo/Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Demo.Models;
+
+namespace Demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Demo.Models.Student> Student { get; set; }
+    }
+}
---
+++ Demo/Demo/Data/Migrations/20210223104339_FistSetUp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo.Data.Migrations
+{
+    public partial class FistSetUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nom = table.Column<string>(nullable: true),
+                    prenom = table.Column<string>(nullable: true),
+                    CIN = table.Column<string>(nullable: true),
+                    adresse = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
