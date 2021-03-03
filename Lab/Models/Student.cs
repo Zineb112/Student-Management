ac457798091f7dc18bce9e3bcb0b6b17955ccece
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-03-03 09:27:59 +0100

    update

---
+++ Lab/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string CIN { get; set; }
+        public string adresse { get; set; }
+    }
+}
